5c7232b4c55c6a4833946a3ddad329f7f49c121b
Cimpean Alexandru <115628859+cimpeanalexandru@users.noreply.github.com>
2023-05-22 16:37:08 +0300

    fix the progress bar mouse drag

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// get our elemnets
+/* Get Our Elements */
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -7,26 +7,24 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-// Build our Functions
+/* Build out functions */
 function togglePlay() {
-  if(video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
 }
 
 function updateButton() {
-  const icon = this.paused ? '►' : '❚❚';
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
   toggle.textContent = icon;
 }
 
 function skip() {
-  video.currentTime += parseFloat(this.dataset.skip);
+ video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
-  video[this.name] = [this.value];
+  video[this.name] = this.value;
 }
 
 function handleProgress() {
@@ -36,11 +34,10 @@ function handleProgress() {
 
 function scrub(e) {
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-  video.currentTime = scrubTime; 
+  video.currentTime = scrubTime;
 }
 
-
-// Hook up the event listeners
+/* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
@@ -55,4 +52,4 @@ let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
